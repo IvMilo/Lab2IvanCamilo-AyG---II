29aeab208f5e628d6df2cbc99ba2d45bb1ff34fa
IvMilo <ivancamilo716@gmail.com>
2024-11-12 23:23:20 -0500

    Subida inicial de los archivos para el laboratorio 2.

---
+++ src/laboratorio1_ivanpeñacastro/Laboratorio1_IvanPeñaCastro.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio1_ivanpeñacastro;
+
+/**
+ *
+ * @author IdkBu
+ */
+public class Laboratorio1_IvanPeñaCastro {
+    
+        public static void main(String[] args) {
+            ChatUi l = new ChatUi();
+            l.setVisible(true);
+            l.setTitle("Ollama");
+            l.setLocationRelativeTo(null);
+    }
+}
